386ef5960dc652ff86e38f5814de403df1c749b2
Ter-web <n.terfa@yahoo.com>
2024-02-08 08:56:46 +0200

    style the hero section

--- app/components/footer/Footer.tsx
+++ app/components/footer/Footer.tsx
@@ -1,7 +1,71 @@
 import React from "react";
+import Container from "../Container";
+import FooterList from "./FooterList";
+import Link from "next/link";
+import { MdFacebook } from "react-icons/md";
+import {
+  AiFillTwitterCircle,
+  AiFillInstagram,
+  AiFillYoutube,
+} from "react-icons/ai";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="bg-slate-700 text-slate-200 text-sm mt-16">
+      <Container>
+        <div className="flex flex-col md:flex-row justify-between pt-16 pb-8">
+          <FooterList>
+            <h3 className="text-base font-bold mb-2">Shop Categories</h3>
+            <Link href="#">Phones</Link>
+            <Link href="#">Laptops</Link>
+            <Link href="#">Desktops</Link>
+            <Link href="#">Watches</Link>
+            <Link href="#">TVs</Link>
+            <Link href="#">Accessories</Link>
+          </FooterList>
+          <FooterList>
+            <h3 className="text-base font-bold mb-2">Customer Service</h3>
+            <Link href="#">Contact Us</Link>
+            <Link href="#">Shipping Policy</Link>
+            <Link href="#">Returns & Exchange</Link>
+            <Link href="#">FAQs</Link>
+          </FooterList>
+          <div className="w-full md:w-1/3 mb-6 md:mb-0">
+            <h3 className="text-base font-bold mb-2">About Us</h3>
+            <p className="mb-2">
+              At our electronics store, we are dedicated to providing the latest
+              and geatest devices and Accessories to our customers. With a wide
+              selection of phones, TVs, laptops, watches, and accessories.
+            </p>
+          </div>
+
+          <FooterList>
+            <h3 className="text-base font-bold mb-2">About Us</h3>
+            <div className="flex gap-2">
+              <Link href="#">
+                <MdFacebook size={24} />
+              </Link>
+              <Link href="#">
+                <AiFillTwitterCircle size={24} />
+              </Link>
+              <Link href="#">
+                <AiFillInstagram size={24} />
+              </Link>
+              <Link href="#">
+                <AiFillYoutube size={24} />
+              </Link>
+            </div>
+          </FooterList>
+        </div>
+        <div className="border-t-2 border-black-500 pt-3 text-center font text-xs">
+          <p>
+            &copy;{new Date().getFullYear()} E-Shop. Terfa-Nomhwange. All rights
+            reserved
+          </p>
+        </div>
+      </Container>
+    </footer>
+  );
 };
 
 export default Footer;
